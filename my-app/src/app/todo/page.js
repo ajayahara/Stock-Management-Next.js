1668c650a6b8da656db9ba8b2e595deaeebe9be6
ajayahara <abak00350@gmail.com>
2023-08-01 18:15:20 +0530

    connect to mongodb using mongo-client(Successfuul)

---
+++ my-app/src/app/todo/page.js
@@ -0,0 +1,5 @@
+export default function Todo(){
+    return (<div className="text-2xl  text-red-800 bg-slate-500 min-h-screen flex justify-center items-center">
+        <h1>Welcome to todo</h1>
+    </div>)
+}
